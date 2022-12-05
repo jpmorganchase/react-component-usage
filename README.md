859aa2f2ace77012de417a10ab58602679c5ccb0
Abedian, Reza <reza.abedian@jpmchase.com>
2022-12-05 11:02:46 +0000

    docs: add licenses, copyright, contributing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# react-component-usage
+## react-component-usage
+
+A tool for UI or Design System teams to scan codebases for occurrences of their React components that match a set of pre-defined rules. The tool is triggered automatically after `npm install` is run on a project that contains npm modules you have authored that have a dependency on `@react-component-usage/postinstall`
 
 Open Source @ JPMorgan Chase
--- packages/core/src/orchestrator.js
+++ packages/core/src/orchestrator.js
@@ -1,3 +1,17 @@
+/* Copyright 2022 J.P. Morgan Chase & Co.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+    the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License. 
+    
+    SPDX-License-Identifier: Apache-2.0 
+*/
+
 const loadRuleConfigs = require('./loadRuleConfigs');
 const findStartFile = require('./findStartFile');
 const getDependencyTree = require('./getDependencyTree');
@@ -38,7 +52,7 @@ async function orchestrator({
 
   const ruleConfigs = await loadRuleConfigs(ruleRequestApiUrl, ruleRequestConfigPath);
   const startFile = findStartFile(entryFile, baseDir);
-  const files = await getDependencyTree(startFile, baseDir); 
+  const files = await getDependencyTree(startFile, baseDir);
 
   for (const file of files) {
     const {ast, language} = parser(file, baseDir);
